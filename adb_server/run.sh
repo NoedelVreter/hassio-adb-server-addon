f4cb4ba396ff86b85a8e1b4f6cb40a0b198e4791
jaaps <236261+jaaps@users.noreply.github.com>
2019-01-17 20:50:28 +0100

    Added some logging

--- adb_server/run.sh
+++ adb_server/run.sh
@@ -2,22 +2,32 @@
 
 DEVICES=$(jq --raw-output '.devices[]' /data/options.json)
 
+echo "Starting up ADB..."
+
 if [ -f /data/adbkey ]; then
+  echo "Found existing private key. Using it."
   cp /data/adbkey /root/.android && cp /data/adbkey.pub /root/.android
 fi
 
-nohup adb -a -P 5037 server nodaemon &
+adb -a -P 5037 server nodaemon > /dev/null 2>&1 &
 
+echo "Server started. Waiting for 30 seconds..."
 sleep 30
 
 if [ ! -f /data/adbkey ]; then
+  echo "Saving newly generated private key."
   cp /root/.android/adbkey /data/adbkey && cp /root/.android/adbkey.pub /data/adbkey.pub
 fi
 
+echo "Connecting to devices."
+for device in $DEVICES; do
+   adb -P 5037 -H 172.17.0.1 connect $device
+done
+echo "Done."
 
 while true; do
   for device in $DEVICES; do
-    adb -P 5037 -H 172.17.0.1 connect $device
+    adb -P 5037 -H 172.17.0.1 connect $device > /dev/null 2>&1
   done
   sleep 60
 done
